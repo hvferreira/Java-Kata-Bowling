24f5dc2a84d26fd752e0b38229c5b05a3c6a83b6
hvferreira <heldervferreira@gmail.com>
2023-02-09 22:31:35 +0000

    strike works

--- src/main/java/org/example/Bowling.java
+++ src/main/java/org/example/Bowling.java
@@ -1,43 +1,79 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Bowling {
 
 
     private final int MAXNUMROLL = 10;//total roll
     private final int CONVERTERSLACH = -1;//total roll
-    private ArrayList<Integer> Strike;
+    private ArrayList<Integer> frame;
     private int total; // total game
+
+    HashMap<Integer, Integer> boolStrike = new HashMap<>();
+    HashMap<Integer, Integer> boolSpace = new HashMap<>();
 
     public Bowling() {
         this.total = 0;
-        Strike = new ArrayList<Integer>();
+        frame = new ArrayList<Integer>();
     }
 
 
     public int total(String rolls) {
-        return sumRolls(rolls.split(" "));
+        sumRolls(rolls.split(" "));
+
+        return sumFrames();
     }
 
     //sum all rools
-    private int sumRolls(String[] arrayRolls) {
+    private void sumRolls(String[] arrayRolls) {
 
         int num = 0;
-        int frame = 0;
-        for (int i = 0; i < arrayRolls.length; i++) {
-            num = convertCharatertoInt(arrayRolls[i]);
 
-            if (num == CONVERTERSLACH) {
-                num = MAXNUMROLL - Integer.parseInt(arrayRolls[i - 1]);
-                //System.out.println(num);
+        for (int i = 0; i < arrayRolls.length; i++) {
+            num = convertCharatertoInt(String.valueOf((arrayRolls[i].charAt(0))));
+
+
+            System.out.println();
+
+            if (num == MAXNUMROLL && arrayRolls[i].charAt(0) == 'X') {
+                boolStrike.put(i, 1);
+                if (boolStrike.containsKey(i - 1)) {
+                    frame.set(i - 1, frame.get(i - 1) + num);
+                    if (boolStrike.containsKey(i - 2)) {
+                        frame.add(num);
+                        //frame.add(num + frame.get(i - 1) + num + frame.get(i - 2) + num);
+                        frame.set(i - 2, frame.get(i - 2) + num);
+                        //frame.set(i - 1, frame.get(i - 1) + num);
+                    } else {
+                        //frame.add(i - 1, frame.get(i - 1) + num);
+                        frame.add(num);
+                    }
+
+                } else {
+                    frame.add(num);
+                }
+
+
+            } else if ((arrayRolls[i].charAt(1)) == CONVERTERSLACH) {
+
+                num = num + MAXNUMROLL - Integer.parseInt(String.valueOf(arrayRolls[i].charAt(0)));
+                frame.add(num);
+            } else {
+
+
+                num = num + convertCharatertoInt(String.valueOf(arrayRolls[i].charAt(1)));
+                if (boolStrike.containsKey(i - 1)) {
+                    frame.set(i - 1, frame.get(i - 1) + num);
+                }
+                frame.add(num);
             }
 
 
-            total = total + num;
         }
 
-        return total;
+
     }
 
     private int convertCharatertoInt(String roll) {
@@ -52,7 +88,10 @@ public class Bowling {
 
     private int sumFrames() {
 
-
+        for (int i : frame) {
+            total = total + i;
+            System.out.print(i + " ");
+        }
         return total;
     }
 }
